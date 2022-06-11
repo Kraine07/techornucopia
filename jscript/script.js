ae9be7d530c06172b8c846eeeb4ae8a63f288b53
Kraine07 <aundre.bushay@gmail.com>
2022-06-10 22:47:53 -0500

    first commit, main and contact pages

---
+++ jscript/script.js
@@ -0,0 +1,15 @@
+var slideIndex = 0;
+slide();
+
+function slide() {
+  var i;
+  var carouselContainer = document.getElementsByClassName("welcome");
+  for (i = 0; i < carouselContainer.length; i++) {
+    carouselContainer[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > carouselContainer.length) {slideIndex = 1}
+  carouselContainer[slideIndex-1].style.display = "block";
+  setTimeout(slide, 5000); // Change image every 2 seconds
+}
+
